4a280566f862ccef5502e7d64a357bc91b4bffec
zhangwenjun <1346502061@qq.com>
2024-02-19 16:07:51 +0800

    Update:Layout

--- src/Extra/App.rs
+++ src/Extra/App.rs
@@ -22,8 +22,36 @@ impl Default for Application {
 
 impl eframe::App for Application {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("顶部")
+            .default_height(20.0)
+            .show(ctx, |ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.heading("顶部");
+                })
+            });
+        egui::TopBottomPanel::bottom("底部")
+            .default_height(20.0)
+            .show(ctx, |ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.heading("底部");
+                })
+            });
+        egui::SidePanel::left("左侧")
+            .default_width(100.0)
+            .show(ctx, |ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.heading("左侧");
+                })
+            });
+        egui::SidePanel::right("右侧").show(ctx, |ui| {
+            ui.centered_and_justified(|ui| {
+                ui.heading("右侧");
+            })
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(self.author.as_str());
+            ui.centered_and_justified(|ui| {
+                ui.heading(self.author.as_str());
+            })
         });
     }
 }
